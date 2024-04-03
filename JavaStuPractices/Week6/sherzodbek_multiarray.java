ecb3ec82173737b5ca46ce789a3ba1205196f9be
Sherzodbek-edu <116169447+Sherzodbek-edu@users.noreply.github.com>
2024-04-03 11:00:23 +0300

    Add files via upload

---
+++ JavaStuPractices/Week6/sherzodbek_multiarray.java
@@ -0,0 +1,11 @@
+public class java {
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3,2,3,2,1}, {4,5,6,2,1,2,7}};
+        for (int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+        }
+        
+    }
+}
